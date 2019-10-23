6df110dfa8e829c05a2d3354861ad0ac31edc57f
Parth Parmar <parthmparmar@gmail.com>
2019-10-22 23:11:22 -0400

    created file structure and all necessary files. Created index.html page with bootstrap to display page.

---
+++ assets/javascript/app.js
@@ -0,0 +1,15 @@
+// Your web app's Firebase configuration
+var firebaseConfig = {
+    apiKey: "AIzaSyDhoW5OTU0G23nP_nqUb4-PAxPS2_p6BOU",
+    authDomain: "oct-19-test-project.firebaseapp.com",
+    databaseURL: "https://oct-19-test-project.firebaseio.com",
+    projectId: "oct-19-test-project",
+    storageBucket: "oct-19-test-project.appspot.com",
+    messagingSenderId: "208957693909",
+    appId: "1:208957693909:web:abc9e402abf2411620675c"
+  };
+  // Initialize Firebase
+  firebase.initializeApp(firebaseConfig);
+  var database = firebase.database;
+
+  
